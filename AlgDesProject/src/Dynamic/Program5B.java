5d7c2ed6aafde86cde9ab00aaf11e51d8500b04f
AJRUMORE <ajrumore1+github@gmail.com>
2024-11-11 14:10:37 -0500

    Milestone 2 - Program 5B Updated

--- AlgDesProject/src/Dynamic/Program5B.java
+++ AlgDesProject/src/Dynamic/Program5B.java
@@ -3,103 +3,117 @@ package Dynamic;
 import java.util.Scanner;
 
 class Program5B {
-    public record Result(int numPlatforms, int totalHeight, int[] numPaintings) {}
+    public record Result(int numShelves, int totalHeight, int[] numPaintings) {}
 
-    // Helper class to store DP state
-    private static class DPState {
+    // Once again, we will be implementing a helper class to store the state of the shelves and paintings
+    private static class MemoState {
         int totalHeight;        // Optimal height for this subproblem
-        int numPlatforms;       // Number of platforms used
+        int numShelves;       // Number of shelves used
         int lastSplit;          // Last split point for reconstruction
 
-        DPState(int height, int platforms, int split) {
+        MemoState(int height, int shelves, int split) {
             totalHeight = height;
-            numPlatforms = platforms;
+            numShelves = shelves;
             lastSplit = split;
         }
     }
-
+    /**
+     * Solution to program 5B
+     * @param n number of paintings
+     * @param w width of the platform
+     * @param heights array of heights of the paintings
+     * @param widths array of widths of the paintings
+     * @return Result object containing the number of platforms, total height of the
+    paintings and the number of paintings on each platform
+     */
     private static Result program5B(int n, int w, int[] heights, int[] widths) {
-        // Create DP table for storing optimal solutions for subproblems
-        DPState[][] dp = new DPState[n][n];
+        // Create memoization table to store the results of previous runs (as learned in class)
+        MemoState[][] memo = new MemoState[n][n];
 
-        // Initialize dp table for single sculpture cases
+        // Initialize memo table for single sculpture cases
         for (int i = 0; i < n; i++) {
-            dp[i][i] = new DPState(heights[i], 1, i);
+            memo[i][i] = new MemoState(heights[i], 1, i);
         }
 
-        // Fill dp table bottom-up
+        // Fill memo table bottom-up
         for (int len = 2; len <= n; len++) {
             for (int start = 0; start <= n - len; start++) {
                 int end = start + len - 1;
 
-                // Try placing all sculptures from start to end on one platform
+                // Try placing all sculptures from start to end on one shelf
                 int currentWidth = 0;
                 int maxHeight = 0;
-                boolean canFitOnePlatform = true;
+                boolean canFitOneShelf = true;
 
                 for (int i = start; i <= end; i++) {
                     currentWidth += widths[i];
                     maxHeight = Math.max(maxHeight, heights[i]);
                     if (currentWidth > w) {
-                        canFitOnePlatform = false;
+                        canFitOneShelf = false;
                         break;
                     }
                 }
 
-                if (canFitOnePlatform) {
-                    dp[start][end] = new DPState(maxHeight, 1, end);
+                if (canFitOneShelf) {
+                    memo[start][end] = new MemoState(maxHeight, 1, end);
                     continue;
                 }
 
-                // Try all possible splits
-                int minTotalHeight = Integer.MAX_VALUE;
-                int bestNumPlatforms = 0;
-                int bestSplit = -1;
+                // Since the rest of the paintings are not able to fit on one shelf, there must be a split somewhere, and we have to find the ideal place to do so
+                int minTotalHeight = 99999; // really large height constraint as default
+                int bestNumShelves = 0; // ideal number of shelves for split
+                int bestSplit = -1; // index of the best split location; -1 for none
 
                 for (int split = start; split < end; split++) {
-                    DPState leftResult = dp[start][split];
-                    DPState rightResult = dp[split + 1][end];
+                    MemoState leftResult = memo[start][split];
+                    MemoState rightResult = memo[split + 1][end];
 
                     int totalHeight = leftResult.totalHeight + rightResult.totalHeight;
 
                     if (totalHeight < minTotalHeight) {
                         minTotalHeight = totalHeight;
-                        bestNumPlatforms = leftResult.numPlatforms + rightResult.numPlatforms;
+                        bestNumShelves = leftResult.numShelves + rightResult.numShelves;
                         bestSplit = split;
                     }
                 }
 
-                dp[start][end] = new DPState(minTotalHeight, bestNumPlatforms, bestSplit);
+                memo[start][end] = new MemoState(minTotalHeight, bestNumShelves, bestSplit);
             }
         }
 
-        // Reconstruct solution to get paintings per platform
-        int[] numPaintings = reconstructSolution(0, n-1, dp);
+        // Reconstruct solution to get paintings per shelf
+        int[] numPaintings = reconstructSolution(0, n-1, memo);
 
-        return new Result(dp[0][n-1].numPlatforms, dp[0][n-1].totalHeight, numPaintings);
+        return new Result(memo[0][n-1].numShelves, memo[0][n-1].totalHeight, numPaintings);
     }
 
     // Helper function to reconstruct the solution
-    private static int[] reconstructSolution(int start, int end, DPState[][] dp) {
+    private static int[] reconstructSolution(int start, int end, MemoState[][] memo) {
+        // Base case
         if (start > end) {
             return new int[0];
         }
 
-        DPState state = dp[start][end];
+        MemoState state = memo[start][end];
         if (state.lastSplit == end) {
-            // All sculptures on one platform
+            // All sculptures on one shelf
             return new int[]{end - start + 1};
         }
 
-        // Combine solutions from left and right sides
-        int[] leftSolution = reconstructSolution(start, state.lastSplit, dp);
-        int[] rightSolution = reconstructSolution(state.lastSplit + 1, end, dp);
+        // recursively calls the function to reconstruct the left and right sides of the solution. leftSolution will hold the number of paintings on each shelf for the left part,
+        // and the rightSolution will hold the number of paintings on each shelf for the right part
+        int[] leftSolution = reconstructSolution(start, state.lastSplit, memo);
+        int[] rightSolution = reconstructSolution(state.lastSplit + 1, end, memo);
 
-        // Combine the solutions
+        // we create an array that will be a return value for the function. This array will hold the shelf counts for both the left and right halves
         int[] result = new int[leftSolution.length + rightSolution.length];
+
+        // fill result with the shelf data from the left half
         for (int i = 0; i < leftSolution.length; i++) {
             result[i] = leftSolution[i];
         }
+
+        // fill result with the shelf data from the right half
         for (int i = 0; i < rightSolution.length; i++) {
             result[leftSolution.length + i] = rightSolution[i];
         }
@@ -120,7 +134,7 @@ class Program5B {
         }
         sc.close();
         Result result = program5B(n, W, heights, widths);
-        System.out.println(result.numPlatforms);
+        System.out.println(result.numShelves);
         System.out.println(result.totalHeight);
         for(int i=0; i<result.numPaintings.length; i++){
             System.out.println(result.numPaintings[i]);
